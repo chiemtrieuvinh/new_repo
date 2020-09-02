7bd4bb5c1967bcde19022015deab755b5f4700e1
trieu.vinh@m1studio.co <trieu.vinh@m1studio.co>
2020-09-02 15:50:26 +0700

    Add cat file

--- Cat.js
+++ Cat.js
@@ -1,6 +1,7 @@
-function Cat(name){
-    
-
-
+function Cat(mouse){
+    this.stomach = []
+    Cat.prototype.eat = function(mouse){
+        this.stomach.push(mouse)
+    }
 }
 module.exports = Cat
