21cd34d0373683e4fc5194430ff4b1a459c5b8a2
trieu.vinh@m1studio.co <trieu.vinh@m1studio.co>
2020-09-03 11:32:51 +0700

    Moving eating function out of object

--- Cat.js
+++ Cat.js
@@ -1,7 +1,8 @@
 function Cat(mouse){
     this.stomach = []
-    Cat.prototype.eat = function(mouse){
-        this.stomach.push(mouse)
-    }
+
+}
+Cat.prototype.eat = function(mouse){
+    this.stomach.push(mouse)
 }
 module.exports = Cat
